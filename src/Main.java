9f2fb6a69f20c8adea76a3d4ff033de6d3b3daf3
Lee, Gwanghyeon <cupid0919@gmail.com>
2024-11-20 10:40:38 +0900

    three

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("201914123 이광현");
         System.out.println(Hello.sum(10));
         System.out.println(Hello.sum(100));
+        System.out.println(Hello.sum(1000));
     }
 }
