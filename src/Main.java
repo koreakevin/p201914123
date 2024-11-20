6b96872bc48eb06c32f2f9df8e6577b079ca4a70
Lee, Gwanghyeon <cupid0919@gmail.com>
2024-11-20 10:47:49 +0900

    six

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import static java.lang.System.out;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("201914123 이광현");
-        System.out.println(Hello.sum(10));
-        System.out.println(Hello.sum(100));
-        System.out.println(Hello.sum(1000));
-        System.out.println(Sansu.max(new int[] { 8, 5, 6, 9, 4, 1, 7, 10, 3, 2, 8 }));
-        System.out.println(Sansu.max(new int[] { -2, 5, 6, 9, -6, 1, 7, 2, 8, 10 }));
+        out.println("201914123 이광현");
+        out.println(Hello.sum(10));
+        out.println(Hello.sum(100));
+        out.println(Hello.sum(1000));
+        out.println(Sansu.max(new int[] {8, 5, 6, 9, 4, 1, 7, 10, 3, 2, 8}));
+        out.println(Sansu.max(new int[] {-2, 5, 6, 9, -6, 1, 7, 2, 8, 10}));
     }
 }
